d83cacac4478c259513f9e62148714e18eea0806
Kürşad <kursadaydogan@pm.me>
2022-11-14 14:29:18 +0300

    contact page fixes

--- api/contact.php
+++ api/contact.php
@@ -1,15 +1,20 @@
 <?php
 
 function handle_msg($name, $email, $msg, $subject) { 
-    $config = json_decode(file_get_contents('../config.json'));
-    $tg_token = $config->bot_token; 
-    $tg_chat_id = $config->tg_user_id; 
+    require_once('../config.php');
+    $tg_token = $config['bot_token']; 
+    $tg_chat_id = $config['user_id']; 
     $tg_base_url = 'https://api.telegram.org/bot' . $tg_token . '/sendMessage'; 
     $msg = "Name: $name\nEmail: $email\nSubject: $subject\n\nMessage: $msg"; 
     $query = 'chat_id=' . $tg_chat_id . '&text=' . urlencode($msg) . '&parse_mode=Markdown'; 
     $tg_url = $tg_base_url . '?' . $query; 
     $response = file_get_contents($tg_url); 
-    return json_decode($response); 
+    $data = json_decode($response); 
+    if ($data->ok) { 
+        return ['ok' => true, 'msg' => 'Message sent successfully!']; 
+    } else { 
+        return ['ok' => false, 'msg' => 'Server error!'];
+    }
 }
 
 
---
+++ example_config.php
@@ -0,0 +1,6 @@
+<?php
+
+$config = array();
+
+$config['bot_token'] = 'YOUR_BOT_TOKEN';
+$config['user_id'] = 'YOUR_USER_ID';
